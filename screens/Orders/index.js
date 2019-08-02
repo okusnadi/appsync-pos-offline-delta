60985baa09e41351ed5deb4291849735f87553e8
Fernando Dingler <fdingler@amazon.com>
2019-08-01 17:07:05 -0700

    Removed unecessary import

--- screens/Orders/index.js
+++ screens/Orders/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { RefreshControl } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import gql from 'graphql-tag';
