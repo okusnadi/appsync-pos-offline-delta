38eb00f57694640ad6d64960b776fc17a8aa90b6
Fernando Dingler <fdingler@amazon.com>
2019-08-05 13:48:54 -0700

    Deleted unused mutations/queries

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,5 +1,4 @@
 // eslint-disable
-// this is an auto generated file. This will be overwritten
 
 export const listOrders = `{
   listOrders(limit: 100) {
@@ -13,6 +12,7 @@ export const listOrders = `{
     }
   }
 }`;
+
 export const getOrder = `query GetOrder($id: ID!) {
   getOrder(id: $id) {
     id
@@ -35,66 +35,7 @@ export const getOrder = `query GetOrder($id: ID!) {
   }
 }
 `;
-// export const listOrders = `query ListOrders(
-//   $filter: ModelOrderFilterInput
-//   $limit: Int
-//   $nextToken: String
-// ) {
-//   listOrders(filter: $filter, limit: $limit, nextToken: $nextToken) {
-//     items {
-//       id
-//       total
-//       subtotal
-//       tax
-//       createdAt
-//       updatedAt
-//       lineItems {
-//         nextToken
-//       }
-//     }
-//     nextToken
-//   }
-// }
-// `;
-export const getLineItem = `query GetLineItem($id: ID!) {
-  getLineItem(id: $id) {
-    id
-    sku
-    qty
-    description
-    price
-    total
-  }
-}
-`;
-export const listLineItems = `query ListLineItems(
-  $filter: ModelLineItemFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listLineItems(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      sku
-      qty
-      description
-      price
-      total
-    }
-    nextToken
-  }
-}
-`;
-export const getProduct = `query GetProduct($id: ID!) {
-  getProduct(id: $id) {
-    id
-    sku
-    name
-    price
-    image
-  }
-}
-`;
+
 export const listProducts = `query ListProducts(
   $filter: ModelProductFilterInput
   $limit: Int
