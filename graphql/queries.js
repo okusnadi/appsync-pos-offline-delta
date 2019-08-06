7b384be3258f606f8b869d87e8a13911bfa1d20e
Fernando Dingler <fdingler@amazon.com>
2019-08-06 16:04:16 -0700

    Updated schema to make createdAt the sort key

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,7 +1,34 @@
 // eslint-disable
+// this is an auto generated file. This will be overwritten
 
-export const listOrders = `{
-  listOrders(limit: 100) {
+export const getOrder = `query GetOrder($id: ID!, $createdAt: String!) {
+  getOrder(id: $id, createdAt: $createdAt) {
+    id
+    total
+    subtotal
+    tax
+    createdAt
+    updatedAt
+    lineItems {
+      nextToken
+    }
+  }
+}
+`;
+export const listOrders = `query ListOrders(
+  $id: ID
+  $createdAt: ModelStringKeyConditionInput
+  $filter: ModelOrderFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listOrders(
+    id: $id
+    createdAt: $createdAt
+    filter: $filter
+    limit: $limit
+    nextToken: $nextToken
+  ) {
     items {
       id
       total
@@ -10,32 +37,49 @@ export const listOrders = `{
       createdAt
       updatedAt
     }
-  }
-}`;
-
-export const getOrder = `query GetOrder($id: ID!) {
-  getOrder(id: $id) {
-    id
-    total
-    subtotal
-    tax
-    createdAt
-    updatedAt
-    lineItems {
-      items {
-        id
-        sku
-        qty
-        description
-        price
-        total
-      }
-      nextToken
-    }
+    nextToken
   }
 }
 `;
-
+export const getLineItem = `query GetLineItem($id: ID!) {
+  getLineItem(id: $id) {
+    id
+    sku
+    qty
+    description
+    price
+    total
+  }
+}
+`;
+export const listLineItems = `query ListLineItems(
+  $filter: ModelLineItemFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listLineItems(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      sku
+      qty
+      description
+      price
+      total
+    }
+    nextToken
+  }
+}
+`;
+export const getProduct = `query GetProduct($id: ID!) {
+  getProduct(id: $id) {
+    id
+    sku
+    name
+    price
+    image
+  }
+}
+`;
 export const listProducts = `query ListProducts(
   $filter: ModelProductFilterInput
   $limit: Int
